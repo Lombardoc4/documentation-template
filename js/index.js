ee618fa1534d1f84497b7c4b508c7fa57729bc37
Cris Lom <bigc4997@gmail.com>
2020-12-19 22:13:07 -0500

    importing json object from js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 (function (doc) {
+  console.log(navigationTitles);
   var $ = {
     // Single Item
     prevButton: doc.getElementById('prev'),
---
+++ js/test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var navigationTitles = {
+  "navigationTitles": ["Welcome!", "Course Objectives", {
+    "Lesson 1: Overview of Sell": ["CellSearch System Componenets", {
+      "CellSearch Circulating Tumor Cell Kit": ["CellSearch Circulating Tumor Cell Kit", "Reagent Storage and Handling"]
+    }, {
+      "The CellSearch CTC Control Kit": "CTC Control Kit Storage and Handling"
+    }, "The CellTracks AutoPrep System", "The CellTracks Analyzer ll", "How the System Works"]
+  }]
+};
